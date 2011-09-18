97136da0c3e9e1c6960887c73e9920c016bf1961
Tim Johnsen <timonus@gmail.com>
2011-09-18 16:15:49 -0700

    Initial Commit

---
+++ SquareApp/SAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAAppDelegate.h
+//  SquareApp
+//
+//  Created by Tim Johnsen on 9/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SquareApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SquareApp
+//
+//  Created by Tim Johnsen on 9/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAAppDelegate class]));
+	}
+}
